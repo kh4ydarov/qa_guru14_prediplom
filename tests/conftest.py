f72ce5488e4b783544fc8af5f5728e28ca3198a7
kh4ydarov <kh4ydarovs@yandex.ru>
2024-05-18 14:18:49 +0500

    hw

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,12 @@ from selene import browser, be, have
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
-# from dotenv import load_dotenv
-#
-#
-# @pytest.fixture(scope='function', autouse=True)
-# def load_env():
-#     load_dotenv()
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope='function', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -18,31 +18,31 @@ def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.base_url = 'https://uzum.uz'
 
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "122.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    #
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
-    #
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    #
-    # browser.config.driver = driver
-    # yield browser
-    #
-    # attach.add_html(browser)
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_video(browser)
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    yield browser
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
 
     browser.quit()
